0a4750cf8791b1008d6614ff53bbeb1e10d65a36
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2022-09-21 10:56:28 -0700

    refactor @bcgov/eslint-config-elmsd -> @bcgov-elmsd/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ const prettierConfig = require("./.prettierrc")
 
 module.exports = {
     root: true,
-    extends: ["@bcgov/eslint-config-elmsd"],
+    extends: ["@bcgov-elmsd/eslint-config"],
     parserOptions: { project: "tsconfig.json", tsconfigRootDir: __dirname, sourceType: "module" },
     rules: {
         "prettier/prettier": ["error", prettierConfig]
